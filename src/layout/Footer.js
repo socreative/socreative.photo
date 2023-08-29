b1b7fa4990962e2bc1833f0159d9888fc506b74d
socreative <socreative@rocketmail.com>
2023-08-29 22:24:38 +0100

    Footer fix

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,33 +5,8 @@ const Footer = () => {
         <div className="container align-center">
           <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-              {/* titles */}
-              <div className="h-titles">
-                <div
-                  className="h-subtitle scrolla-element-anim-1 scroll-animate"
-                  data-animate="active"
-                >
-                  Address:
-                </div>
-                <div
-                  className="h-text scrolla-element-anim-1 scroll-animate"
-                  data-animate="active"
-                >
-                  Poole, <br />
-                  Dorset, UK
-                </div>
-              </div>
             </div>
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-              {/* titles */}
-              <div className="h-titles">
-                <div
-                  className="h-subtitle scrolla-element-anim-1 scroll-animate"
-                  data-animate="active"
-                >
-                  Follow Us:
-                </div>
-              </div>
               {/* social */}
               <div className="social-links footer-social-links">
                 <a
@@ -58,25 +33,6 @@ const Footer = () => {
                 >
                   <i aria-hidden="true" className="fab fa-twitter" />
                 </a>
-              </div>
-            </div>
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-              {/* titles */}
-              <div className="h-titles">
-                <div
-                  className="h-subtitle scrolla-element-anim-1 scroll-animate"
-                  data-animate="active"
-                >
-                  Get in touch:
-                </div>
-                <div
-                  className="h-text scrolla-element-anim-1 scroll-animate"
-                  data-animate="active"
-                >
-                  <p>
-                    socreativephoto@gmail.com
-                  </p>
-                </div>
               </div>
             </div>
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
