0aa7f4fb9980cf3033f2300f6253321f9546bb4a
socreative <socreative@rocketmail.com>
2023-09-17 20:15:20 +0100

    Adds sitemap and removes all other pages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import { Fragment, useEffect, useState } from 'react';
 import Preloader from '../src/layout/Preloader';
 import '../styles/globals.css';
