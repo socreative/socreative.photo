8e51001bf153aca91da6450c8053d5552a7e30b2
socreative <socreative@rocketmail.com>
2024-02-13 07:51:30 +0000

    Adds larger container

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -34,19 +34,19 @@ const Footer = () => {
                   <i aria-hidden="true" className="fab fa-twitter" />
                 </a>
               </div>
-              <span class="contact-telephone">Tel: 07466318246</span>
+              <span className="contact-telephone">Tel: 07466318246</span>
             </div>
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 text-center">
               
             </div>
-            {/* <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
               <div
                 className="copyright-text scrolla-element-anim-1 scroll-animate"
                 data-animate="active"
               >
                 © 2024 Socreative Photography
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
